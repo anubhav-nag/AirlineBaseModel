ed7b4a2f8427300c9c1ea38d3208ceb91e8f2ad5
Anubhav Nag <anubhav.nag_cs18@gla.ac.in>
2022-04-12 04:30:22 +0530

    Entity/Flight
    added all the columns of flight table in Flight

--- src/main/java/com/naehas/springassignment/dao/FlightsRepository.java
+++ src/main/java/com/naehas/springassignment/dao/FlightsRepository.java
@@ -1,9 +1,11 @@
 package com.naehas.springassignment.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.naehas.springassignment.entities.Flight;
 
+@RepositoryRestResource(path="members")
 public interface FlightsRepository extends JpaRepository<Flight, Integer> {
 
 }
--- src/main/java/com/naehas/springassignment/entities/Flight.java
+++ src/main/java/com/naehas/springassignment/entities/Flight.java
@@ -1,5 +1,56 @@
 package com.naehas.springassignment.entities;
 
-public class Flight {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="flights")
+public class Flight {
+	
+	// added all columns of the flight table
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="airline")
+	private String airline;
+	
+	@Column(name="flight_No")
+	private String flightNo;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAirline() {
+		return airline;
+	}
+
+	public void setAirline(String airline) {
+		this.airline = airline;
+	}
+
+	public String getFlightNo() {
+		return flightNo;
+	}
+
+	public void setFlightNo(String flightNo) {
+		this.flightNo = flightNo;
+	}
+
+	@Override
+	public String toString() {
+		return "Flight [id=" + id + ", airline=" + airline + ", flightNo=" + flightNo + "]";
+	}
+	
 }
