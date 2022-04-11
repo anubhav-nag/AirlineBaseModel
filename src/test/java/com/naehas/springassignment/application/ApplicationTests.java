7bd27d7d7f60346fea4ef84338bbdae6705963f7
Anubhav Nag <anubhav.nag_cs18@gla.ac.in>
2022-04-12 03:51:47 +0530

    Added maven project airline-model-assignment

---
+++ src/test/java/com/naehas/springassignment/application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naehas.springassignment.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
