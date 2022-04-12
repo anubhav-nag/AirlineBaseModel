5d9946500efbd40e18151af38624fde5f9e7f90c
Anubhav Nag <anubhav.nag_cs18@gla.ac.in>
2022-04-13 02:52:44 +0530

    Create FlightRouteServiceImplementation.java
    Created an implementation class for flight route services

---
+++ src/main/java/com/naehas/springassignment/service/implementation/FlightRouteServiceImplementation.java
@@ -0,0 +1,54 @@
+package com.naehas.springassignment.service.implementation;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.naehas.springassignment.dao.FlightRouteRepository;
+import com.naehas.springassignment.entities.FlightRoute;
+import com.naehas.springassignment.service.FlightRouteService;
+
+@Service
+public class FlightRouteServiceImplementation implements FlightRouteService {
+
+	private FlightRouteRepository flightRouteRepository;
+	
+	@Autowired
+	public FlightRouteServiceImplementation(FlightRouteRepository theFlightRouteRepository) {
+		flightRouteRepository = theFlightRouteRepository;
+	}
+	
+	@Override
+	public List<FlightRoute> findAll() {
+		return flightRouteRepository.findAll();
+	}
+
+	@Override
+	public FlightRoute findById(int flightRouteId) {
+		Optional<FlightRoute> flightRoute = flightRouteRepository.findById(flightRouteId);
+		
+		FlightRoute theFlightRoute = null;
+		
+		if(flightRoute.isPresent()) {
+			theFlightRoute = flightRoute.get();
+		}
+		else {
+			throw new RuntimeException("Did not found the flight route details of id - " + flightRouteId);
+		}
+		
+		return theFlightRoute;
+	}
+
+	@Override
+	public void save(FlightRoute flightRoute) {
+		flightRouteRepository.save(flightRoute);
+	}
+
+	@Override
+	public void deleteById(int flightRouteId) {
+		flightRouteRepository.deleteById(flightRouteId);
+	}
+
+}
