a63f57d73569a4bc3863836939d695f0cf460935
Anubhav Nag <anubhav.nag_cs18@gla.ac.in>
2022-04-13 02:44:10 +0530

    Create FlightRouteService.java

---
+++ src/main/java/com/naehas/springassignment/service/FlightRouteService.java
@@ -0,0 +1,17 @@
+package com.naehas.springassignment.service;
+
+import java.util.List;
+
+import com.naehas.springassignment.entities.FlightRoute;
+
+public interface FlightRouteService {
+	
+	public List<FlightRoute> findAll();
+	
+	public FlightRoute findById(int flightRouteId);
+	
+	public void save(FlightRoute flightRoute);
+	
+	public void deleteById(int flightRouteId);
+	
+}
