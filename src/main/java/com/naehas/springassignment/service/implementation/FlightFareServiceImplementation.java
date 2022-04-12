3c7cb88f91de8019869f88b7e5571b73d0dc07b5
Anubhav Nag <anubhav.nag_cs18@gla.ac.in>
2022-04-13 03:03:18 +0530

    Create FlightFareServiceImplementation.java
    Created an implementation file for flightfare service interface

---
+++ src/main/java/com/naehas/springassignment/service/implementation/FlightFareServiceImplementation.java
@@ -0,0 +1,54 @@
+package com.naehas.springassignment.service.implementation;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.naehas.springassignment.dao.FlightFareRepository;
+import com.naehas.springassignment.entities.FlightFare;
+import com.naehas.springassignment.service.FlightFareService;
+
+@Service
+public class FlightFareServiceImplementation implements FlightFareService {
+
+	private FlightFareRepository flightFareRepository;
+	
+	@Autowired
+	public FlightFareServiceImplementation(FlightFareRepository theFlightFareRepository) {
+		flightFareRepository = theFlightFareRepository; 
+	}
+	
+	@Override
+	public List<FlightFare> findAll() {
+		return flightFareRepository.findAll();
+	}
+
+	@Override
+	public FlightFare findById(int flightFareId) {
+		Optional<FlightFare> flightFare = flightFareRepository.findById(flightFareId);
+		
+		FlightFare fare = null;
+		
+		if(flightFare.isPresent()) {
+			fare = flightFare.get();
+		}
+		else {
+			throw new RuntimeException("Did not got the flight's fare details for the id- " + flightFareId);
+		}
+		
+		return fare;
+	}
+
+	@Override
+	public void save(FlightFare flightFare) {
+		flightFareRepository.save(flightFare);
+	}
+
+	@Override
+	public void deleteById(int flightFareId) {
+		flightFareRepository.deleteById(flightFareId);
+	}
+
+}
