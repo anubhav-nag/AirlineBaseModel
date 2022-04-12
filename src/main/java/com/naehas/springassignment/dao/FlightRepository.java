d2f2d88c40f1742bb0679ca1acea27efafbb40b0
Anubhav Nag <anubhav.nag_cs18@gla.ac.in>
2022-04-13 02:18:59 +0530

    Update FlightRepository.java
    removed repositoryrestresource annotation

--- src/main/java/com/naehas/springassignment/dao/FlightRepository.java
+++ src/main/java/com/naehas/springassignment/dao/FlightRepository.java
@@ -1,11 +1,9 @@
 package com.naehas.springassignment.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.naehas.springassignment.entities.Flight;
 
-//@RepositoryRestResource(path="flightdetail")
 public interface FlightRepository extends JpaRepository<Flight, Integer> {
 
 }
