a37d4064fe657d1d4b64d5adc9f59b38964f2020
Anubhav Nag <anubhav.nag_cs18@gla.ac.in>
2022-04-13 02:22:54 +0530

    Create FlightService.java
    Added a flight service interface

---
+++ src/main/java/com/naehas/springassignment/service/FlightService.java
@@ -0,0 +1,16 @@
+package com.naehas.springassignment.service;
+
+import java.util.List;
+
+import com.naehas.springassignment.entities.Flight;
+
+public interface FlightService {
+
+	public List<Flight> findAll();
+	
+	public Flight findById(int flightId);
+	
+	public void save(Flight flight);
+	
+	public void deleteById(int flightId);
+}
